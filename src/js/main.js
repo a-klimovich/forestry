d2831666f2ab2f8512cca814e5b07edc212d8c71
a-klimovich <ar2r.klimovich@gmail.com>
2021-12-15 22:17:53 +0100

    init / add: base style & components

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 (function () {
-  console.log(12);
+  document.querySelectorAll("nav.navbar li").forEach(item => {
+    item.onmouseover = item.onmouseout = handler;
+
+    function handler(event) {
+      if (event.type == 'mouseover') {
+        item.querySelector('.dropdown-list') !== null 
+        ? item.querySelector('.dropdown-list').style.display = 'block'
+        : false
+      }
+
+      if (event.type == 'mouseout') {
+        item.querySelector('.dropdown-list') !== null 
+        ? item.querySelector('.dropdown-list').style.display = 'none'
+        : false
+      }
+    }
+  })
 })()
