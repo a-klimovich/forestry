7eaff951c2f7da988d4fcdde355662cf9edd1b67
a-klimovich <ar2r.klimovich@gmail.com>
2021-12-21 20:19:11 +0100

    final fix

--- src/js/main.js
+++ src/js/main.js
@@ -37,4 +37,60 @@
     });
   }
   // MAP INIT END
+
+  // SLIDER
+  $(document).ready(function(){
+    const $overlay = $(`<div class="overlay"><div>`)
+    const $sliderCloser = $('#close-button')
+    const $sliderMain = $('.galery-slider')
+    const $tourismGalery = document.querySelectorAll('.tourism-and-rest-page__galery .galery-iamge')
+    const $animalGalery = document.querySelectorAll('.animal-item-page__galery .galery-iamge')
+    const $newsItemGalery = document.querySelectorAll('.news-item-page__galery .galery-image')
+    
+    let arr = [$tourismGalery, $animalGalery, $newsItemGalery]
+    let arrGalleryImage = []
+
+    arr.map((item) => {
+      return item.length > 0 ? arrGalleryImage.push(...item) : []
+    })
+    
+    let isShownSlider = false
+
+    $sliderMain.slick({
+      slidesToShow: 1,
+      variableWidth: true,
+      centerMode: true,
+      adaptiveHeight: true,
+      prevArrow: `<button type="button" id="previous-button" aria-label="Previous" class="baguetteBox-button"><svg width="44" height="60"><polyline points="30 10 10 30 30 50" stroke="rgba(255,255,255,0.5)" stroke-width="4" stroke-linecap="butt" fill="none" stroke-linejoin="round"></polyline></svg></button>`,
+      nextArrow: `<button type="button" id="next-button" aria-label="Next" class="baguetteBox-button"><svg width="44" height="60"><polyline points="14 10 34 30 14 50" stroke="rgba(255,255,255,0.5)" stroke-width="4" stroke-linecap="butt" fill="none" stroke-linejoin="round"></polyline></svg></button>`,
+    })
+
+    arrGalleryImage.forEach((item, idx) => {
+      item.addEventListener('click', () => {
+        isShownSlider = !isShownSlider
+
+        SliderShown(isShownSlider, idx)
+      })
+    })
+
+    function SliderShown (isShown, currentSlide) {
+      if (isShown === true ) {
+        $overlay.appendTo(document.body);
+        $('body').css('overflow-y', 'hidden')
+        $sliderCloser.css("display", "block")
+        $sliderMain.css("display", "block")
+        $sliderMain.slick('slickGoTo', currentSlide);
+      } else {
+        $overlay.remove()
+        $('body').css('overflow-y', 'auto')
+        $sliderCloser.css("display", "none")
+        $sliderMain.css("display", "none")
+      }
+    }
+
+    $sliderCloser.on('click', () => {
+      SliderShown(isShownSlider = false)
+    })
+  });
+  // SLIDER END
 })()
