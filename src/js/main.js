f6efb055f47465b38d0a82b6c44682a5c290cbe4
a-klimovich <ar2r.klimovich@gmail.com>
2022-01-24 23:39:15 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,69 +1,94 @@
 (function () {
-  // NAVIGATION MENU 
-  const headerMenuListItems = document.querySelectorAll("nav .navbar li")
-  const mobileMenuBtn = document.querySelector('#mobileNavMenuBtn')
-  const navBar = document.querySelector("nav > .navbar")
+  // NAVIGATION MENU
+  const headerMenuListItems = document.querySelectorAll("nav .navbar li");
+  const mobileMenuBtn = document.querySelector("#mobileNavMenuBtn");
+  const navBar = document.querySelector("nav > .navbar");
 
-  headerMenuListItems.forEach(item => {
-    const NavItem = item.querySelector('.dropdown-list')
-    const NavBtn = item.querySelector('.dropbtn')
+  headerMenuListItems.forEach((item) => {
+    const NavItem = item.querySelector(".dropdown-list");
+    const NavBtn = item.querySelector(".dropbtn");
 
-    item.addEventListener('click', () => {
-      const DropList = document.querySelectorAll('.navbar .dropdown-list.active')
-      const DropBtn = document.querySelectorAll('.navbar .dropbtn.active')
+    item.addEventListener("click", () => {
+      const DropList = document.querySelectorAll(
+        ".navbar .dropdown-list.active"
+      );
+      const DropBtn = document.querySelectorAll(".navbar .dropbtn.active");
 
       if (DropList.length > 0 && DropBtn.length > 0) {
-        DropList.forEach(i => {
-          i.classList.toggle('active')
-        })
-        DropBtn.forEach(i => {
-          i.classList.toggle('active')
-        })
+        DropList.forEach((i) => {
+          i.classList.toggle("active");
+        });
+        DropBtn.forEach((i) => {
+          i.classList.toggle("active");
+        });
       }
-      
+
       if (NavItem !== null) {
-        NavItem.classList.toggle('active')
-        NavBtn.classList.toggle('active')
+        NavItem.classList.toggle("active");
+        NavBtn.classList.toggle("active");
       }
-    })
-  })
+    });
+  });
 
-  window.addEventListener('click', e => {
-    const target = e.target
-    const menuDropdownList = document.querySelectorAll('.dropdown-list')
-    if (!target.closest('nav') && !target.closest('.dropdown-list')) {
-      menuDropdownList.forEach(i => i.classList.remove('active'))
-      document.querySelector('.navbar button.dropbtn.active').removeClass('active')
-      mobileMenuBtn.classList.remove('active')
-      navBar.classList.remove('active')
+  window.addEventListener("click", (e) => {
+    const target = e.target;
+    const menuDropdownList = document.querySelectorAll(".dropdown-list");
+    if (!target.closest("nav") && !target.closest(".dropdown-list")) {
+      menuDropdownList.forEach((i) => i.classList.remove("active"));
+      document
+        .querySelector(".navbar button.dropbtn.active")
+        .removeClass("active");
+      mobileMenuBtn.classList.remove("active");
+      navBar.classList.remove("active");
     }
-  })
+  });
 
-  mobileMenuBtn.addEventListener('click', () => {
-    mobileMenuBtn.classList.toggle('active')
-    navBar.classList.toggle('active')
-  })
+  mobileMenuBtn.addEventListener("click", () => {
+    mobileMenuBtn.classList.toggle("active");
+    navBar.classList.toggle("active");
+  });
   // NAVIGATION MENU END
 
   // MAP INIT
-  if (document.querySelector('#map')) {
+  if (document.querySelector("#map")) {
     ymaps.ready(init);
-      function init(){
-        let myMap = new ymaps.Map("map", {
-          center: [52.227608372181884, 24.358953455256273],
-          zoom: 10,
+    function init() {
+      let myMap = new ymaps.Map("map", {
+        center: [52.227608372181884, 24.358953455256273],
+        zoom: 10,
       });
     }
   }
   // MAP INIT END
 
   // GALERY
-  baguetteBox.run('.baguetteBoxGalery');
+  baguetteBox.run(".baguetteBoxGalery");
 
-  baguetteBox.run('.baguetteBoxSoloItem', {
-    captions: function(element) {
-        return element.getElementsByTagName('img')[0].alt;
-    }
+  baguetteBox.run(".baguetteBoxSoloItem", {
+    captions: function (element) {
+      return element.getElementsByTagName("img")[0].alt;
+    },
   });
   // GALERY END
-})()
+
+  // File add
+  // let updateList = () => {
+
+  // }
+
+  document.querySelector("#file").addEventListener("change", (event) => {
+    let input = document.getElementById("file");
+    let output = document.getElementById("fileList");
+    let children = "";
+    for (let i = 0; i < input.files.length; ++i) {
+      children +=
+        "<li>" +
+        input.files.item(i).name +
+        '<span class="remove-list" onclick="return this.parentNode.remove()">X</span>' +
+        "</li>";
+    }
+    output.innerHTML = children;
+  });
+
+  // File add END
+})();
